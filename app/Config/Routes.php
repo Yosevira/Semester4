2a2276033e31aa953a005c378cf4b18a1634ef66
Yosevira <yoseviramaulidinarahma@saintek.unipdu.ac.id>
2025-07-01 22:06:33 +0700

    Membuat Grafik di Ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Profil::index');
+//Tugas Pak Fajrul
+// $routes->get('/', 'Profil::index'); 
 $routes->get('/about','Page::about');
 $routes->get('/contact','Page::contact');
 $routes->get('/faqs', 'Page::faqs');
@@ -17,6 +18,7 @@ $routes->get('/mjk','mata_kuliah::mjk');
 $routes->get('/rpl','mata_kuliah::rpl');
 $routes->get('/mbd','mata_kuliah::mbd');
 
+//Tugas Pak Miftah
 $routes->get('/books','Books::index');
 $routes->get('/books/detail/(:segment)','Books::detail/$1');
 $routes->delete('/books/delete/(:num)', 'Books::delete/$1');
@@ -27,5 +29,10 @@ $routes->post('/books/save', 'Books::save');
 
 $routes->get('/penulis','Penulis::index');
 $routes->get('/penulis/detail/(:segment)','Penulis::detail/$1');
+
+//grafik
+$routes->get('/', 'Home::index');
+$routes->get('home/apiData/(:num)', 'Home::apiData/$1');
+
 
 $routes->setAutoRoute(false);
